b42afbe3948b4380382496aa32d541b284dbe7f7
OverjoyedMaxwel <kanapod.lam@student.mahidol.edu>
2025-04-12 13:47:52 +0000

    WriteReadToFile2

--- LL_test.cpp
+++ LL_test.cpp
@@ -9,24 +9,13 @@ using namespace std;
 int main(int argc, char *argv[])
 {  
    int choice;
-   cout << "Type \"1\" = Write to file \"2\" = Read to file" << endl ;
+   cout << "Type \"1\" = Write to file \"2\" = Read to file \"3\" = Delete file Data"<< endl ;
    cin >> choice ;
+   
    if(choice == 1){   
-      ofstream fout("demo4.txt");
+      ofstream fout("demo4.txt", ios::app);
       if (fout) {  
-/*     
-        LL A;
-       int i;
-       NODE *t;
-
-       for(i=1;i<argc;i+=4) {
-                         string n=argv[i+3];   
-                         t=new student(atoi(argv[i]),atoi(argv[i+1]),atof(argv[i+2]),n);
-                         A.add_node(t);
-                            }
-       A.show_all();
-        //highestGPA(t);
-*/      
+      
        cout << "successfully write to text file." << endl;
        for (int i = 1; i < argc; i+=4) {
             string n=argv[i+3];
@@ -36,9 +25,10 @@ int main(int argc, char *argv[])
        fout << endl;
       } else {
          cout << "error" << endl;
-    }
+      }
       fout.close();   
    }
+   
    else if(choice == 2){
       ifstream fin("demo4.txt");
       if (!fin) {
@@ -65,5 +55,15 @@ int main(int argc, char *argv[])
       fin.close();
     
    } 
+   
+   else if (choice == 3) {
+      ofstream fout("demo4.txt", ios::trunc); // ล้างไฟล์
+      if (fout) {
+          cout << "File cleared successfully." << endl;
+      } else {
+          cerr << "Failed to clear file." << endl;
+      }
+      fout.close();
+  }
   return 0;
 }
